8eb0c5c34ed2df67eb32b27147bdd7df3691f69c
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-10-05 13:21:23 +0200

    Update part2.c

--- lab1/part2.c
+++ lab1/part2.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/ipc.h>
+# include <sys/msg.h>
 
 // structure of the message queue
 struct message_queue{
