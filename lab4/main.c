9c84d4de6563227c666eea7355e074bd149aefca
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-01 20:40:30 +0800

    Update main.c

--- lab4/main.c
+++ lab4/main.c
@@ -1,166 +1,204 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
 
-#define CYLINDER 5000
-#define REQUESTS 1000
+# define CYLINDERS 200
+# define REQUESTS    8
 
-int fcfs(int* req, int init);
-int sstf(int* req, int init);
-int scan(int* req, int init);
-int cscan(int* req, int init);
-int look(int* req, int init);
-int clook(int* req, int init);
-
-int req[REQUESTS];
-
-int compare(const void* a, const void* b){    
-    if(*((int *)a) > *((int *)b)) return 1;
-    if(*((int *)a) < *((int *)b)) return -1;
-    return 0;
+int fcfs(int* req, int head){
+    int move = 0;
+    int curr = head;
+    for(int i = 0; i < REQUESTS; i++){
+        move += abs(req[i] - curr);
+        curr = req[i];
+    }
+    
+    return move;
 }
 
-int fcfs(int* req, int init){
-    int res = 0;
-    for(int i = 0, curr = init; i < REQUESTS; curr = req[i++]) res += abs(curr - req[i]);
+int sstf(int* req, int head){
+    int move = 0;
+    int visited[REQUESTS] = {0};
     
-    return res;
-}
-
-int sstf(int* req, int init){
-    int res = 0;
-    int adj = 0;
-    
-    int* sorted_req = malloc(REQUESTS * sizeof(int));
-    memcpy(sorted_req, req, REQUESTS * sizeof(int));
-    qsort(sorted_req, REQUESTS, sizeof(*req), compare);
-    while(abs(sorted_req[adj + 1] - init) <= abs(sorted_req[adj] - init)) adj++;
-
-    res = abs(sorted_req[adj] - init);
-    int curr_val = sorted_req[adj];
-    int l_pos = adj - 1;
-    int r_pos = adj + 1;
-
-    while(l_pos > 0 || r_pos < REQUESTS - 1){
-        int l_diff = l_pos > 0 ? abs(sorted_req[l_pos - 1] - curr_val) : CYLINDER + 1;
-        int r_diff = r_pos < (REQUESTS - 1) ? abs(sorted_req[r_pos + 1] - curr_val) : CYLINDER + 1;
-
-        if(l_diff < r_diff){
-            res += l_diff;
-            l_pos--;
-            curr_val = sorted_req[l_pos];
+    for(int i = 0; i < REQUESTS; i++){
+        int shortest = INT_MAX;
+        int diff;
+        int next = -1;
+        
+        for(int j = 0; j < REQUESTS; j++){
+            if(!visited[j]){
+                diff = abs(head - req[j]);
+                if(diff < shortest){
+                    shortest = diff;
+                    next = j;
+                }
+            }
         }
-        else{
-            res += r_diff;
-            r_pos++;
-            curr_val = sorted_req[r_pos];
+        
+        if(next != -1){
+            head = req[next];
+            move += shortest;
+            visited[next] = 1;
         }
     }
 
-    return res;
+    return move;
 }
 
-int scan(int* req, int init){
-    int res = 0;
-    int adj = 0;
-    int curr = init;
-
+int scan(int* req, int head){
+    int move = 0;
     int* sorted_req = malloc(REQUESTS * sizeof(int));
     memcpy(sorted_req, req, REQUESTS * sizeof(int));
-    qsort(sorted_req, REQUESTS, sizeof(*req), compare);
-    while(abs(sorted_req[adj + 1] - init) <= abs(sorted_req[adj] - init)) adj++;
-
-    for(int i = adj; i >= 0 && adj >= 0; i--){
-        res += abs(sorted_req[i] - curr);
-        curr = sorted_req[i];
+    
+    for(int i = 0; i < REQUESTS; i++){
+        for(int j = 0; j < REQUESTS - i - 1; j++){
+            if(sorted_req[j] > sorted_req[j + 1]){
+                int tmp = sorted_req[j];
+                sorted_req[j] = sorted_req[j + 1];
+                sorted_req[j + 1] = tmp;
+            }
+        }
     }
 
-    res += curr;
-    curr = 0;
-    for(int i = adj + 1; i < REQUESTS && adj < REQUESTS; i++){
-        res += abs(sorted_req[i] - curr);
-        curr = sorted_req[i];
+    int pos = 0;
+    while(pos < REQUESTS && sorted_req[pos] < head) pos++;
+    int direction = head < CYLINDERS / 2 ? -1 : 1;
+    pos = direction > 1 ? pos : pos - 1;
+    int opposite = pos - direction;
+    
+    while(1){
+        move += abs(head - sorted_req[pos]);
+        head = sorted_req[pos];
+        
+        if(pos == 0 || pos == REQUESTS - 1) break;
+        pos += direction;
     }
     
-    return res;
-}
-
-int cscan(int* req, int init){
-    int res = 0;
-    int adj = 0;
-
-    int* sorted_req = malloc(REQUESTS * sizeof(int));
-    memcpy(sorted_req, req, REQUESTS * sizeof(int));
-    qsort(sorted_req, REQUESTS, sizeof(*req), compare);
-    while(abs(sorted_req[adj + 1] - init) <= abs(sorted_req[adj] - init)) adj++;
-
-    int curr = sorted_req[adj];
-    for(int i = adj, j = 0; j < REQUESTS; j++){
-        int pos = i++ % REQUESTS;
-        res += abs(sorted_req[pos] - curr);
-        curr = sorted_req[pos];
-    }
-
-    res += (2 * CYLINDER) - sorted_req[REQUESTS - 1] + sorted_req[0];
-
-    return res;
-}
-
-int look(int* req, int init){
-    int res = 0;
-    int adj = 0;
-    int curr = init;
-
-    int* sorted_req = malloc(REQUESTS * sizeof(int));
-    memcpy(sorted_req, req, REQUESTS * sizeof(int));
-    qsort(sorted_req, REQUESTS, sizeof(*req), compare);
-    while(abs(sorted_req[adj + 1] - init) <= abs(sorted_req[adj] - init)) adj++;
-
-    for(int i = adj; i >= 0 && adj >= 0; i--){
-        res += abs(sorted_req[i] - curr);
-        curr = sorted_req[i];
-    }
-
-    for(int i = adj + 1; i < REQUESTS && adj < REQUESTS; i++){
-        res += abs(sorted_req[i] - curr);
-        curr = sorted_req[i];
+    direction *= -1;
+    move += direction > 0 ? head : CYLINDERS - head - 1;
+    head = direction > 0 ? 0 : CYLINDERS - 1;
+    pos = opposite;
+    
+    while(pos >= 0 && pos < REQUESTS){
+        move += abs(head - sorted_req[pos]);
+        head = sorted_req[pos];
+        pos += direction;
     }
     
-    return res;
+    return move;
 }
 
-int clook(int* req, int init){
-    int res = 0;
-    int adj = 0;
-    int curr = init;
-
+int cscan(int* req, int head){
+    int move = 0;
     int* sorted_req = malloc(REQUESTS * sizeof(int));
     memcpy(sorted_req, req, REQUESTS * sizeof(int));
-    qsort(sorted_req, REQUESTS, sizeof(*req), compare);
-    while(abs(sorted_req[adj + 1] - init) <= abs(sorted_req[adj] - init)) adj++;
+    
+    for(int i = 0; i < REQUESTS; i++){
+        for(int j = 0; j < REQUESTS - i - 1; j++){
+            if(sorted_req[j] > sorted_req[j + 1]){
+                int tmp = sorted_req[j];
+                sorted_req[j] = sorted_req[j + 1];
+                sorted_req[j + 1] = tmp;
+            }
+        }
+    }
 
-    for(int i = adj, j = 0; j < REQUESTS; j++){
-        res += abs(sorted_req[i % REQUESTS] - curr);
-        curr = sorted_req[i++ % REQUESTS];
+    int pos = 0;
+    while(pos < REQUESTS && sorted_req[pos] < head) pos++;
+    // int direction = head < CYLINDERS / 2 ? -1 : 1;
+    int direction = 1;
+    pos = direction > 1 ? pos : pos - 1;
+    int opposite = pos - direction;
+    
+    while(1){
+        move += abs(head - sorted_req[pos]);
+        head = sorted_req[pos];
+        
+        if(pos == 0 || pos == REQUESTS - 1) break;
+        pos += direction;
     }
     
-    return res;
+    printf("%d\n", move);
+    
+    move += direction > 0 ? CYLINDERS - head - 1 : head;
+    head = direction > CYLINDERS - 1 ? 0 : 0;
+    pos = direction > CYLINDERS - 2 ? 0 : 1;
+    
+    printf("%d\n", move);
+    
+    while(pos >= 0 && pos < REQUESTS){
+        move += abs(head - sorted_req[pos]);
+        if(pos == opposite) break;
+        head = sorted_req[pos];
+        pos += direction;
+    }
+    printf("%d\n", move);
+    
+    return move;
+}
+
+int look(int* req, int head){
+    int move = 0;
+    int* sorted_req = malloc(REQUESTS * sizeof(int));
+    memcpy(sorted_req, req, REQUESTS * sizeof(int));
+    
+    for(int i = 0; i < REQUESTS; i++){
+        for(int j = 0; j < REQUESTS - i - 1; j++){
+            if(sorted_req[j] > sorted_req[j + 1]){
+                int tmp = sorted_req[j];
+                sorted_req[j] = sorted_req[j + 1];
+                sorted_req[j + 1] = tmp;
+            }
+        }
+    }
+    
+    int pos = 0;
+    while(pos < REQUESTS && sorted_req[pos] < head) pos++;
+    int direction = head < CYLINDERS / 2 ? -1 : 1;
+    pos = direction > 1 ? pos : pos - 1;
+    int opposite = pos - direction;
+    
+    while(1){
+        move += abs(head - sorted_req[pos]);
+        head = sorted_req[pos];
+        
+        if(pos == 0 || pos == REQUESTS - 1) break;
+        pos += direction;
+    }
+    
+    direction *= -1;
+    head = direction > 0 ? head : CYLINDERS - head - 1;
+    pos = opposite;
+    
+    while(pos >= 0 && pos < REQUESTS){
+        move += abs(head - sorted_req[pos]);
+        head = sorted_req[pos];
+        pos += direction;
+    }
+    
+    return move;
+}
+
+int clook(int* req, int head){
+
+
 }
 
 int main(int argc, char** argv){
     if(argc < 2) exit(EXIT_FAILURE);
     if(atoi(argv[1]) > 4999) exit(EXIT_FAILURE);
 
-    int init = atoi(argv[1]);
-    for(int i = 0; i < REQUESTS; i++) req[i] = rand() % CYLINDER;
+    int head = atoi(argv[1]);
+    int req[REQUESTS] = {98, 183, 37, 122, 14, 124, 65, 67};
+    // for(int i = 0; i < REQUESTS; i++) req[i] = rand() % CYLINDERS;
     
-    printf("Total head movement\n");
-    printf("FCFS : %d\n", fcfs(req, init));
-    printf("SSTF : %d\n", sstf(req, init));
-    printf("SCAN : %d\n", scan(req, init));
-    printf("CSCAN: %d\n", cscan(req, init));
-    printf("LOOK : %d\n", look(req, init));
-    printf("CLOOK: %d\n", clook(req, init));
+    printf("FCFS  head movement: %d\n", fcfs(req, head));
+    printf("SSTF  head movement: %d\n", sstf(req, head));
+    printf("SCAN  head movement: %d\n", scan(req, head));
+    printf("CSCAN head movement: %d\n", cscan(req, head));
+    printf("LOOK  head movement: %d\n", look(req, head));
+    // printf("CLOOK head movement: %d\n", clook(req, head));
     
     return 0;
 }
